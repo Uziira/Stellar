f4eecaa4514c6242f4bd9566c0e9e623453e1fb1
Aishiiteru <119226399+Uziira@users.noreply.github.com>
2023-12-19 11:52:09 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-if (window.innerWidth <= 900) {
+if (window.innerWidth <= 1100) {
     alert("L'expérience sur téléphone n'est pas optimale. Nous vous recommandons de visiter le site depuis un ordinateur.");
 }
