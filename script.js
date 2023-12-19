d9643006aa557387f397e7f4c5e1ea4dd524fccd
Aishiiteru <119226399+Uziira@users.noreply.github.com>
2023-12-19 16:01:33 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-if (window.innerWidth <= 1600) {
+if (window.innerWidth <= 1500) {
     alert("The phone experience is not optimal. We recommend that you visit the site from a computer.");
 }
+
+const typed = new Typed('#animatedTitle', {
+      strings: ['Stellar'],
+      typeSpeed: 100,
+    });
+
+const typed2 = new Typed('#informationText', {
+    strings: ['Explore the infinite mystery of the universe and share our passion for space exploration'],
+    typeSpeed: 50,
+  });
