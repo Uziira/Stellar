b0f3b1355e8006ef46e30b44309e187157cc4bb8
Aishiiteru <119226399+Uziira@users.noreply.github.com>
2023-12-19 11:14:27 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    alert("L'expérience sur téléphone n'est pas optimale. Nous vous recommandons de visiter le site depuis un ordinateur.");
+}
