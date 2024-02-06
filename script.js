8575a1e4730ddcc71f2fc93a8a6363b0b3cb1b18
Aishiiteru <119226399+Uziira@users.noreply.github.com>
2024-02-06 21:22:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if (window.innerWidth <= 1500) {
-    alert("The phone experience is not optimal. We recommend that you visit the site from a computer.");
+    alert("The experience is not optimal on little screens. We recommend that you visit the site from a screen computer.");
 }
 
 const typed = new Typed('#animatedTitle', {
